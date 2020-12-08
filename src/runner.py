03f2c4b7c67352cdf3c08fc9cf8f54bf3c81fcee
Wanderson Sousa <wandersonsousa3004@gmail.com>
2020-12-08 17:23:03 -0300

    now extract only valid proxies

--- src/runner.py
+++ src/runner.py
@@ -2,25 +2,7 @@ from scrap_proxies import scrape_freeproxylists
 
 
 
-# freeProxyListsScraping = scrape_freeproxylists()
+freeProxyListsScraping = scrape_freeproxylists()
 
 
-# freeProxyListsScraping.request()
-
-from bs4 import BeautifulSoup
-import requests
-def getProxies():
-    r = requests.get('https://free-proxy-list.net/')
-    soup = BeautifulSoup(r.content, 'html.parser')
-    table = soup.find('tbody')
-    proxies = []
-    for row in table:
-        if row.find_all('td')[4].text =='elite proxy':
-            proxy = ':'.join([row.find_all('td')[0].text, row.find_all('td')[1].text])
-            proxies.append(proxy)
-        else:
-            pass
-    return proxies
-
-
-print( getProxies() )
+freeProxyListsScraping.get_valid_proxies()
